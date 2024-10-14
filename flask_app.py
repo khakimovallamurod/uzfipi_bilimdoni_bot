264b7f795f45a51a04f5855cce8dcd6a1d60ecca
xakimovallamurod <allamurodxakimov@gmail.com>
2024-10-14 20:12:44 +0500

    flask api

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from telegram import Update
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler, MessageHandler, Filters  # importlar to'liq
 from config import get_token
 
 app = Flask(__name__)
@@ -11,11 +11,13 @@ bot = Application.builder().token(TOKEN).build()
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
-    print(data)
-    update = Update.de_json(request.get_json(), bot.bot)
+    print(data)  # Webhookga kelayotgan ma'lumotlarni konsolga chiqarish
+
+    # Telegram bot yangilanishini yaratish
+    update = Update.de_json(data, bot.bot)
     bot.process_update(update)
-    return jsonify({'statust': 200})
+    return jsonify({'status': 200})  # 'statust' ni 'status' ga o'zgartirish
 
 # Flask ilovasi
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Flask ilovasini ma'lum portda ishga tushirish
